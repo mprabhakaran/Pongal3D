c2b4dcbc51fc11f89c185cae7715db9a3242d35a
mprabhakaran <m.prabhakaran1991@gmail.com>
2022-01-14 02:57:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.136.0';
 
 const scene = new THREE.Scene();
-console.log(scene);
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100);
+scene.add(camera);
+camera.position.z = 3;
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxBufferGeometry();
+const material = new THREE.MeshBasicMaterial({color:0xff0000});
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube);
+
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+}
+animate();
