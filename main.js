ec70f16399de3a339b04894dd3abd1d96b2029a3
mprabhakaran <m.prabhakaran1991@gmail.com>
2022-01-14 03:03:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,100);
 scene.add(camera);
-camera.position.z = 3;
+camera.position.z = 5;
 
 
 const renderer = new THREE.WebGLRenderer();
