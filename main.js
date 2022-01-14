449481dd7cb0596bcf39c80fa3112d4cd1e55bf7
mprabhakaran <m.prabhakaran1991@gmail.com>
2022-01-14 13:43:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -63,6 +63,12 @@ window.addEventListener('resize',()=>{
   
   });
 
+window.addEventListener('load',()=>{
+    setTimeout(()=>{
+        window.scrollTo(0,1);
+    },0);
+});
+
 const controls = new OrbitControls(camera,renderer.domElement);
 
 
